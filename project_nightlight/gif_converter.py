7856ab9bbd345f8ca8bb8a735e9d3f2c3528c03d
joltex <anders.klaus@abcellera.com>
2019-09-15 12:29:02 -0700

    Re-order functions and rename get_rgb_map_from_frame() -> get_rgb_map_from_image()

--- project_nightlight/gif_converter.py
+++ project_nightlight/gif_converter.py
@@ -2,6 +2,38 @@ import json
 from PIL import Image, GifImagePlugin
 
 
+def get_rgb_map_from_gif(gif_file):
+    # Create an Image object from the gif file.
+    gif_obj = Image.open(gif_file)
+    # gif_obj = gif_obj.convert('RGB')
+
+    # Iterate through each frame of the gif and read the RGB values for each pixel.
+    rgb_map = []
+    for frame in range(0, gif_obj.n_frames):
+        gif_obj.seek(frame)
+        rgb_map.append(get_rgb_map_from_image(gif_obj))
+
+    return rgb_map
+
+
+def get_rgb_map_from_image(img_obj):
+    """ Parse the RGB value for each pixel in a Pillow Image object
+
+    :param img_obj: Pillow Image object.
+    :return: List where each element is a list of RGB values representing one row
+             of the image.
+    """
+    rgb_map = []
+    width, height = img_obj.size
+    for y in range(height):
+        row_rgb_map = []
+        for x in range(width):
+            row_rgb_map.append(img_obj.getpixel((x, y)))
+        rgb_map.append(row_rgb_map)
+
+    return rgb_map
+
+
 def write_rgb_map_to_file(rgb_map, outfile, pretty=False):
     """ Write an RGB map to a text file
 
@@ -25,35 +57,3 @@ def write_rgb_map_to_file_pretty(rgb_map, outfile):
             for row in frame:
                 json.dump(row, fout)
                 fout.write('\n')
-
-
-def get_rgb_map_from_gif(gif_file):
-    # Create an Image object from the gif file.
-    gif_obj = Image.open(gif_file)
-    # gif_obj = gif_obj.convert('RGB')
-
-    # Iterate through each frame of the gif and read the RGB values for each pixel.
-    rgb_map = []
-    for frame in range(0, gif_obj.n_frames):
-        gif_obj.seek(frame)
-        rgb_map.append(get_rgb_map_from_frame(gif_obj))
-
-    return rgb_map
-
-
-def get_rgb_map_from_frame(img_obj):
-    """ Parse the RGB value for each pixel in a Pillow Image object
-
-    :param img_obj: Pillow Image object.
-    :return: List where each element is a list of RGB values representing one row
-             of the image.
-    """
-    rgb_map = []
-    width, height = img_obj.size
-    for y in range(height):
-        row_rgb_map = []
-        for x in range(width):
-            row_rgb_map.append(img_obj.getpixel((x, y)))
-        rgb_map.append(row_rgb_map)
-
-    return rgb_map
