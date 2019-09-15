2d7c5e1c0c19e9f895e42ef9088ebaeb453d130b
joltex <anders.klaus@abcellera.com>
2019-09-15 12:25:03 -0700

    Add pretty writing of rgb map to file

--- project_nightlight/gif_converter.py
+++ project_nightlight/gif_converter.py
@@ -2,9 +2,29 @@ import json
 from PIL import Image, GifImagePlugin
 
 
-def write_rgb_map_to_file(rgb_map, outfile):
+def write_rgb_map_to_file(rgb_map, outfile, pretty=False):
+    """ Write an RGB map to a text file
+
+    :param rgb_map: RGB map data structure - nested list where 1st level = frames of a video,
+                    2nd level = rows of a frame, 3rd level = RGB values of a row.
+    :param outfile: Path to file to write the RGB map to.
+    :param pretty: If True, write the RGB map to the file using newlines to separate each row of
+                   each frame.
+    """
+    if pretty:
+        write_rgb_map_to_file_pretty(rgb_map, outfile)
+    else:
+        with open(outfile, 'w') as fout:
+            json.dump(rgb_map, fout)
+
+
+def write_rgb_map_to_file_pretty(rgb_map, outfile):
     with open(outfile, 'w') as fout:
-        json.dump(rgb_map, fout, indent=2)
+        for i, frame in enumerate(rgb_map):
+            fout.write(f'# Frame {i+1}\n')
+            for row in frame:
+                json.dump(row, fout)
+                fout.write('\n')
 
 
 def get_rgb_map_from_gif(gif_file):
@@ -22,9 +42,9 @@ def get_rgb_map_from_gif(gif_file):
 
 
 def get_rgb_map_from_frame(img_obj):
-    """ Parse the RGB value for each pixel in a pillow image object
+    """ Parse the RGB value for each pixel in a Pillow Image object
 
-    :param img_obj:
+    :param img_obj: Pillow Image object.
     :return: List where each element is a list of RGB values representing one row
              of the image.
     """
