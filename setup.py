18770b40fa9f83bb605d6663413a37993785f83c
joltex <anders.klaus@abcellera.com>
2019-09-21 16:33:35 -0700

    Update dependencies and entry_points

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project_nightlight',
+    name='nightlight',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['pillow'],
-    # entry_points={'console_scripts': ['convert = project_nightlight.converter:main']},
+    install_requires=['adafruit-circuitpython-lis3dh',
+                      'pillow'],
+    entry_points={'console_scripts': ['convert = nightlight.converter:main']},
     url='https://github.com/joltex/project_nightlight',
     license='',
     author='',
